89551c758688ffbee9aabcff4685ef3720548ab5
컴퓨터 하는 라따뚜이 <81808703+junisYun@users.noreply.github.com>
2021-08-28 13:16:43 +0900

    auto increase project count when upload the project
    프로젝트 추가시 카테고리에 따라 자동으로 카운트 증가

--- main.js
+++ main.js
@@ -1,128 +1,122 @@
-'use strict'
+"use strict";
 // make navbar transparent when it is on the top
-const navbar = document.querySelector('#navbar');
+const navbar = document.querySelector("#navbar");
 const navbarHeight = navbar.getBoundingClientRect().height;
-document.addEventListener('scroll', () => {
-    if (window.scrollY > navbarHeight) {
-        navbar.classList.add('navbar--dark');
-    } else {
-        navbar.classList.remove('navbar--dark');
-    }
-})
+document.addEventListener("scroll", () => {
+  if (window.scrollY > navbarHeight) {
+    navbar.classList.add("navbar--dark");
+  } else {
+    navbar.classList.remove("navbar--dark");
+  }
+});
 
 // handle scrolling when tapping on the navbar menu
-const navbarMenu = document.querySelector('.navbar__menu');
-navbarMenu.addEventListener('click', (event) => {
-    const target = event.target;
-    const link = target.dataset.link;
-    if (link == null) {
-        return;
-    }
-    const scrollTo = document.querySelector(link);
-    scrollTo.scrollIntoView(
-        {
-            behavior: 'smooth'
-        }
-    );
-    navbarMenu.classList.remove('open');
+const navbarMenu = document.querySelector(".navbar__menu");
+navbarMenu.addEventListener("click", (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link == null) {
+    return;
+  }
+  const scrollTo = document.querySelector(link);
+  scrollTo.scrollIntoView({
+    behavior: "smooth",
+  });
+  navbarMenu.classList.remove("open");
 });
 
 // handle scrolling when tapping Contact button
-const query = document.querySelector('.home__contact');
-query.addEventListener('click', () => {
-    const scrollTo = document.querySelector('#contact');
-    scrollTo.scrollIntoView(
-        {
-            behavior: 'smooth'
-        }
-    );
-})
+const query = document.querySelector(".home__contact");
+query.addEventListener("click", () => {
+  const scrollTo = document.querySelector("#contact");
+  scrollTo.scrollIntoView({
+    behavior: "smooth",
+  });
+});
 
-// disappear navbar when wheel go up 
+// disappear navbar when wheel go up
 let cnt = 0;
-const body = document.querySelector('body');
-body.addEventListener('wheel', (event) => {
-    wheelEvent(event);
-})
+const body = document.querySelector("body");
+body.addEventListener("wheel", (event) => {
+  wheelEvent(event);
+});
 const wheelEvent = (event) => {
-    if (window.scrollY < 100) {
-        navbar.classList.remove('navbar--wheelDown');
-        return;
-    }
-    if (event.deltaY < 0) {
-        cnt++;
-    } else {
-        cnt = 0;
-    }
-    if (cnt < 5) {
-        navbar.classList.add('navbar--wheelDown');
-        navbarMenu.classList.remove('open');
-    } else {
-        navbar.classList.remove('navbar--wheelDown');
-    }
-}
+  if (window.scrollY < 100) {
+    navbar.classList.remove("navbar--wheelDown");
+    return;
+  }
+  if (event.deltaY < 0) {
+    cnt++;
+  } else {
+    cnt = 0;
+  }
+  if (cnt < 5) {
+    navbar.classList.add("navbar--wheelDown");
+    navbarMenu.classList.remove("open");
+  } else {
+    navbar.classList.remove("navbar--wheelDown");
+  }
+};
 
 // go to top when click the sideNav
-const sideNav = document.querySelector('.sideNav');
-sideNav.addEventListener('click', () => {
-    scrollIntoView('#home');
-})
+const sideNav = document.querySelector(".sideNav");
+sideNav.addEventListener("click", () => {
+  scrollIntoView("#home");
+});
 
-
-// show sideNav 
-document.addEventListener('scroll', () => {
-    if (window.scrollY > 687) {
-        sideNav.style.opacity = 1;
-        sideNav.style.pointerEvents = "auto";
-    } else {
-        sideNav.style.opacity = 0;
-        sideNav.style.pointerEvents = "none";
-    }
-})
+// show sideNav
+document.addEventListener("scroll", () => {
+  if (window.scrollY > 687) {
+    sideNav.style.opacity = 1;
+    sideNav.style.pointerEvents = "auto";
+  } else {
+    sideNav.style.opacity = 0;
+    sideNav.style.pointerEvents = "none";
+  }
+});
 
 // adjust opacity of home background when scroll down
-const home = document.querySelector('#home');
+const home = document.querySelector("#home");
 const homeHeight = home.getBoundingClientRect().height;
-document.addEventListener('scroll', (e) => {
-    home.style.opacity = `${1 - window.scrollY / homeHeight}`;
-})
+document.addEventListener("scroll", (e) => {
+  home.style.opacity = `${1 - window.scrollY / homeHeight}`;
+});
 
 // scrollTo custom function
 const scrollIntoView = (selector) => {
-    const scrollTo = document.querySelector(selector);
-    scrollTo.scrollIntoView(
-        {
-            behavior: "smooth"
-        }
-    );
-}
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({
+    behavior: "smooth",
+  });
+};
 
-const workBtnContainer = document.querySelector('.work__categories');
-const workProjectContainer = document.querySelector('.work__projects');
-const projects = document.querySelectorAll('.project');
-workBtnContainer.addEventListener('click', (e) => {
-    const filter = e.target.dataset.category || e.target.parentNode.dataset.category;
-    if (filter === undefined) return;
-    workProjectContainer.classList.add('animation-out');
+const workBtnContainer = document.querySelector(".work__categories");
+const workProjectContainer = document.querySelector(".work__projects");
+const projects = document.querySelectorAll(".project");
+workBtnContainer.addEventListener("click", (e) => {
+  const filter =
+    e.target.dataset.category || e.target.parentNode.dataset.category;
+  if (filter === undefined) return;
+  workProjectContainer.classList.add("animation-out");
 
-    console.log(e.target.parentNode);
-    const active = document.querySelector('.category__btn.active');
-    active.classList.remove('active');
-    const target = e.target.nodeName === 'BUTTON' ? e.target : e.target.parentNode;
-    target.classList.add('active');
+  console.log(e.target.parentNode);
+  const active = document.querySelector(".category__btn.active");
+  active.classList.remove("active");
+  const target =
+    e.target.nodeName === "BUTTON" ? e.target : e.target.parentNode;
+  target.classList.add("active");
 
-
-    setTimeout(() => {
-        projects.forEach((project) => {
-            if (filter === 'all' || filter === project.dataset.category) {
-                project.classList.remove('invisible');
-            } else {
-                project.classList.add('invisible');
-            }
-        })
-        workProjectContainer.classList.remove('animation-out');
-    }, 300);
-})
+  setTimeout(() => {
+    projects.forEach((project) => {
+      if (filter === "all" || filter === project.dataset.category) {
+        project.classList.remove("invisible");
+      } else {
+        project.classList.add("invisible");
+      }
+    });
+    workProjectContainer.classList.remove("animation-out");
+  }, 300);
+});
 
 // const aboutmeHeight = document.getElementById('about').offsetTop - 1;
 // const skillsHeight = document.getElementById('skills').offsetTop - 1;
@@ -151,8 +145,29 @@ workBtnContainer.addEventListener('click', (e) => {
 //         document.querySelector('.navbar__menu__item.award').classList.remove('active');
 //     }
 // });
-const navbartoggle = document.querySelector('.navbar__toggleBtn');
-navbartoggle.addEventListener('click', (e) => {
-    navbarMenu.classList.toggle('open');
-    console.log(e.target);
-})
+const navbartoggle = document.querySelector(".navbar__toggleBtn");
+navbartoggle.addEventListener("click", (e) => {
+  navbarMenu.classList.toggle("open");
+  console.log(e.target);
+});
+
+const getCountProject = (category) => {
+  let cnt = 0;
+  projects.forEach((item) => {
+    if (item.dataset.category === category) {
+      cnt++;
+    }
+  });
+  return cnt;
+};
+
+document.querySelector(".category__count.all").innerText =
+  getCountProject("frontend") +
+  getCountProject("backend") +
+  getCountProject("embeded");
+document.querySelector(".category__count.frontend").innerText =
+  getCountProject("frontend");
+document.querySelector(".category__count.backend").innerText =
+  getCountProject("backend");
+document.querySelector(".category__count.embeded").innerText =
+  getCountProject("embeded");
